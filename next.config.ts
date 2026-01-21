cec90dbc0a72fe2595cdcb3fb1413e980b3d3ed0
kotchaniphapk <kotchanipha.pk@gmail.com>
2026-01-21 11:30:10 +0700

    fixed image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // Disable image optimization for better compatibility in production
+  },
 };
 
 export default nextConfig;
