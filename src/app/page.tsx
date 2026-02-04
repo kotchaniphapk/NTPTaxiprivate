96ee65e34c5e3f59e6eb60a9e703a0dc506d5d72
kotchaniphapk <kotchanipha.pk@gmail.com>
2026-02-04 14:01:45 +0700

    add google Script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { VehiclesSection } from "@/components/VehiclesSection";
 import { OneDayTripSection } from "@/components/OneDayTripSection";
 import { GallerySection } from "@/components/GallerySection";
 import { FinalCTASection } from "@/components/FinalCTASection";
+import Script from "next/script";
 
 
 export default function Home() {
@@ -14,6 +15,24 @@ export default function Home() {
 
   return (
     <div className="">
+      <Script
+        id="gtag-loader"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17930048716"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17930048716');
+          `,
+        }}
+      />
       <Navbar />
       <HeroSection />
       <VehiclesSection />
